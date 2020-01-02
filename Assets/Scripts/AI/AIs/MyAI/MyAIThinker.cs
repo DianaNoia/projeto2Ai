915f40d2a7dfadb46a50a24479a8628cd76928e1
IssaMaia <Diogo_maia99@hotmail.com>
2020-01-02 00:30:48 +0000

    Plays to win if 3 in line on vertical

--- Assets/Scripts/AI/AIs/MyAI/MyAIThinker.cs
+++ Assets/Scripts/AI/AIs/MyAI/MyAIThinker.cs
@@ -13,15 +13,22 @@ public class MyAIThinker : IThinker
 
     private System.Random random;
 
+    private List<Pos> enemyPiece = new List<Pos>();
+    private List<Pos> myPiece = new List<Pos>();
+
     public FutureMove Think(Board board, CancellationToken ct)
     {
         // The move to perform
         FutureMove move;
+        FutureMove? test;
+
+        test = Play(board);
+        if (test != null)
+        {
+            return (FutureMove)test;
+        }
 
         random = new System.Random();
-
-        //This method is not working
-        Check(board);
 
         // Check how many pieces current player has
         int roundPieces = board.PieceCount(board.Turn, PShape.Round);
@@ -68,7 +75,6 @@ public class MyAIThinker : IThinker
             {
                 move = FutureMove.NoMove;
             }
-            return move;
         }
         else
         {
@@ -87,7 +93,10 @@ public class MyAIThinker : IThinker
             {
                 move = FutureMove.NoMove;
             }
-        }       
+        }
+
+        Check(board);
+        Debug.Log($"My pieces {myPiece.Count + 1} | Enemie Pieces {enemyPiece.Count + 1}");
 
         // Return move
         return move;
@@ -96,21 +105,104 @@ public class MyAIThinker : IThinker
     private void Check(Board board)
     {
         Piece piece;
+        Pos pos;
+        bool hasPiece;
 
-        for(int i = 0; i >= board.rows; i++)
+        for(int i = 0; i < board.rows ; i++)
         {
-            for(int z = 0; i >= board.cols; z++)
+            for(int z = 0; z < board.cols; z++)
             {
                 if(board[i,z] != null)
                 {
-                    piece = (Piece)board[i, z];
-                    if(piece.color == board.Turn)
+                    hasPiece = false;
+                    piece = (Piece)board[i,z];
+                    pos = new Pos(i, z);
+                    if (piece.color == board.Turn)
                     {
-                        Debug.Log($"Found one of my Pieces at {i} | {z}");
+                        foreach(Pos position in myPiece)
+                        {
+                            if(position.col == pos.col && position.row == pos.row)
+                            {
+                                hasPiece = true;
+                            }
+                        }
+
+                        if (!hasPiece)
+                        {
+                            myPiece.Add(pos);
+                        }
+                        
+                    }else if(piece.color != board.Turn)
+                    {
+                        foreach (Pos position in enemyPiece)
+                        {
+                            if (position.col == pos.col && position.row == pos.row)
+                            {
+                                hasPiece = true;
+                            }
+                        }
+
+                        if (!hasPiece)
+                        {
+                            enemyPiece.Add(pos);
+                        }
                     }
                 }
             }
         }
+    }
+
+    private FutureMove? Play(Board board)
+    {
+        FutureMove? move = default;
+        int[] rows = new int[board.rows * board.cols];
+        int[] cols = new int[board.rows * board.cols];
+        int i = 0;
+
+        foreach(Pos pos in myPiece)
+        {            
+            rows.SetValue(pos.row, i);
+            cols.SetValue(pos.col, i);
+            i++;
+
+        }
+
+        for(int x = 0; x < board.cols; x++)
+        {
+            move = CheckCols(board, x);
+        }
+
+        return move;
+
+    }
+
+    private FutureMove? CheckCols(Board board, int col)
+    {
+        FutureMove move;
+        List<bool> threeInLine = new List<bool>(3);
+        Piece piece;  
+
+        for (int i = 0; i < board.rows; i++)
+        {
+            if(board[i, col] == null && !(threeInLine.Count == 3))
+            {
+                return null;
+            }else if (board[i, col] == null && (threeInLine.Count == 3))
+            {
+                return move = new FutureMove(col, PShape.Round);
+            }
+            piece = (Piece)board[i, col];
+            if(piece.color == board.Turn)
+            {
+                threeInLine.Add(true);
+            }
+            else
+            {
+                threeInLine.RemoveRange(0, threeInLine.Count);
+            }            
+        }
+
+        return null;
     }
 
 }
